6218b8fab1e5f44653961dc382d117137dd8cebd
Sami Maachi <samovitch360@gmail.com>
2024-06-27 02:35:04 +0100

    feat: Setup swagger for API documentation

--- app/Http/Controllers/Api/v1/TestController.php
+++ app/Http/Controllers/Api/v1/TestController.php
@@ -5,8 +5,25 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+
+/**
+ * @OA\Info(
+ *     title="Pet Shop API",
+ *     version="1.0",
+ *     description="Documentation for v1 api of pet store",
+ * )
+ */
 class TestController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     @OA\Response(
+     *         response="200",
+     *         description="Fetch all users"
+     *     )
+     * )
+     */
     public function index()
     {
         return User::all();
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\Api\v1\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [TestController::class, 'index']);
+
+
+
+Route::get('/users', [TestController::class, 'index']);
